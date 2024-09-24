7e50eecd70253c5920ccf54be200849800fe2da1
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-09-24 12:48:58 -0700

    standardize code formatting

--- data-updates.lua
+++ data-updates.lua
@@ -2,42 +2,42 @@
 local multiplier = tonumber(settings.startup["eco-friendly-factor"].value)
 
 local settings = {
-  ["oil-processing"] = not settings.startup["eco-friendly-cheicals"].value,
-  ["chemistry"] = not settings.startup["eco-friendly-cheicals"].value,
-  ["smelting"] = not settings.startup["eco-friendly-furnaces"].value,
-  ["crafting-with-fluid"] = not settings.startup["eco-friendly-assembling-machines"].value,
-  ["crafting"] = not settings.startup["eco-friendly-assembling-machines"].value,
-  ["basic-crafting"] = not settings.startup["eco-friendly-assembling-machines"].value,
-  ["advanced-crafting"] = not settings.startup["eco-friendly-assembling-machines"].value,
-  ["mining-drill"] = not settings.startup["eco-friendly-drills"].value,
+    ["oil-processing"] = not settings.startup["eco-friendly-cheicals"].value,
+    ["chemistry"] = not settings.startup["eco-friendly-cheicals"].value,
+    ["smelting"] = not settings.startup["eco-friendly-furnaces"].value,
+    ["crafting-with-fluid"] = not settings.startup["eco-friendly-assembling-machines"].value,
+    ["crafting"] = not settings.startup["eco-friendly-assembling-machines"].value,
+    ["basic-crafting"] = not settings.startup["eco-friendly-assembling-machines"].value,
+    ["advanced-crafting"] = not settings.startup["eco-friendly-assembling-machines"].value,
+    ["mining-drill"] = not settings.startup["eco-friendly-drills"].value,
 }
 
 local function is_blacklisted(prototype)
-  local output = false
-  if prototype.crafting_categories then
-    for _, category in pairs(prototype.crafting_categories) do
-      if settings[category] == true then
-        output = true
-      end
+    local output = false
+    if prototype.crafting_categories then
+        for _, category in pairs(prototype.crafting_categories) do
+            if settings[category] == true then
+                output = true
+            end
+        end
     end
-  end
-  if settings[prototype.type] then
-    output = true
-  end
-  return output
+    if settings[prototype.type] then
+        output = true
+    end
+    return output
 end
 
 for each, type in pairs(data.raw) do
-  for every, prototype in pairs(type) do
-    if prototype.energy_source and prototype.energy_source.type == "electric" then
-      if prototype.energy_source.emissions_per_minute then
-        local emissions_per_minute = tonumber(prototype.energy_source.emissions_per_minute)
-        if emissions_per_minute and emissions_per_minute > 0 then
-          if not is_blacklisted(prototype) then
-            prototype.energy_source.emissions_per_minute = emissions_per_minute * multiplier
-          end
+    for every, prototype in pairs(type) do
+        if prototype.energy_source and prototype.energy_source.type == "electric" then
+            if prototype.energy_source.emissions_per_minute then
+                local emissions_per_minute = tonumber(prototype.energy_source.emissions_per_minute)
+                if emissions_per_minute and emissions_per_minute > 0 then
+                    if not is_blacklisted(prototype) then
+                        prototype.energy_source.emissions_per_minute = emissions_per_minute * multiplier
+                    end
+                end
+            end
         end
-      end
     end
-  end
 end
--- settings.lua
+++ settings.lua
@@ -1,48 +1,48 @@
 
 local eco_friendly_factor = {
-  type = "string-setting",
-  name = "eco-friendly-factor",
-  setting_type = "startup",
-  default_value = "0.00",
-  allowed_values = {
-    "0.00", "0.25", "0.50", "0.75", "1.00"
-  },
-  order = "a"
+    type = "string-setting",
+    name = "eco-friendly-factor",
+    setting_type = "startup",
+    default_value = "0.00",
+    allowed_values = {
+        "0.00", "0.25", "0.50", "0.75", "1.00"
+    },
+    order = "a"
 }
 
 local eco_friendly_cheicals = {
-  type = "bool-setting",
-  name = "eco-friendly-cheicals",
-  setting_type = "startup",
-  default_value = true,
-  order = "b"
+    type = "bool-setting",
+    name = "eco-friendly-cheicals",
+    setting_type = "startup",
+    default_value = true,
+    order = "b"
 }
 local eco_friendly_furnaces = {
-  type = "bool-setting",
-  name = "eco-friendly-furnaces",
-  setting_type = "startup",
-  default_value = true,
-  order = "b"
+    type = "bool-setting",
+    name = "eco-friendly-furnaces",
+    setting_type = "startup",
+    default_value = true,
+    order = "b"
 }
 local eco_friendly_drills = {
-  type = "bool-setting",
-  name = "eco-friendly-drills",
-  setting_type = "startup",
-  default_value = true,
-  order = "b"
+    type = "bool-setting",
+    name = "eco-friendly-drills",
+    setting_type = "startup",
+    default_value = true,
+    order = "b"
 }
 local eco_friendly_assembling_machines = {
-  type = "bool-setting",
-  name = "eco-friendly-assembling-machines",
-  setting_type = "startup",
-  default_value = true,
-  order = "b"
+    type = "bool-setting",
+    name = "eco-friendly-assembling-machines",
+    setting_type = "startup",
+    default_value = true,
+    order = "b"
 }
 
 data:extend({
-  eco_friendly_factor,
-  eco_friendly_cheicals,
-  eco_friendly_furnaces,
-  eco_friendly_drills,
-  eco_friendly_assembling_machines
+    eco_friendly_factor,
+    eco_friendly_cheicals,
+    eco_friendly_furnaces,
+    eco_friendly_drills,
+    eco_friendly_assembling_machines
 })
